20a953f4d2b6a38189b008c7f32d33517ae9729c
Ian Mayton <ian.mayton@gmail.com>
2020-07-24 20:51:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,8 +34,7 @@ class Song
   end
   
   def self.find_by_name(name) 
-    @@all.each do |song|
-      if song.name == name 
+    @@all.include? {|song| song.name == name}
         return song 
       end
     end
