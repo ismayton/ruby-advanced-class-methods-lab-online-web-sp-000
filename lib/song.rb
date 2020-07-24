e24cb0e0f30e709e892c512cd03f53f97454e6fe
Ian Mayton <ian.mayton@gmail.com>
2020-07-24 20:29:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -27,6 +29,7 @@ class Song
   def self.create_by_name(name)
     song = self.new(name)
     @@all << song
+    binding.pry
   end
   
   def self.find_by_name(name) 
