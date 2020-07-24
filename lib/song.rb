962df401f9cef0cc0eb99cd079001aad760382ef
Ian Mayton <ian.mayton@gmail.com>
2020-07-24 20:38:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,12 +24,13 @@ class Song
   
   def self.new_by_name(name)
     song = self.new(name)
+    @@all << song
   end
 
   def self.create_by_name(name)
     song = self.new(name)
-    save
     @@all << song
+    song 
   end
   
   def self.find_by_name(name) 
