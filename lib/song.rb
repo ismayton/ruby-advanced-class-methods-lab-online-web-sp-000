4a6d3bd39a52c3359168c3cd09e1567eb8c6657b
Ian Mayton <ian.mayton@gmail.com>
2020-07-24 21:05:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,11 +44,11 @@ class Song
   
   def self.find_or_create_by_name(name) 
     if self.find_by_name(name)
-      return self.find_by_name(name) 
+      song = self.find_by_name(name) 
     else
-      self.create_by_name(name)
+      song = self.create_by_name(name)
     end
-    nil
+    song
   end 
   
   def self.alphabetical 
